ce754a2fbff2b18bd6a62fd795f2eb61cf75cadd
UmutKzl <261115766+UmutKzl@users.noreply.github.com>
2026-06-29 20:00:34 +0300

    chore: remove title from homepage because we already have that in navbar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,8 @@
 export default function Homepage() {
   return (
     <>
-      <h1 className="text-3xl text-center mx-auto w-fit pt-5 pb-5">dotshowcase</h1>
+
+      <br />
 
       <p className="text-center">
         Welcome to dotshowcase. <br />

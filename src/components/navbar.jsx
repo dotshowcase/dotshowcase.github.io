2cd888b46216ee81e4abb206d0a7256f2934c39f
UmutKzl <261115766+UmutKzl@users.noreply.github.com>
2026-06-29 19:04:56 +0300

    chore: fix header

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="bg-neutral-900 p-4 border-b border-gray-700">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-xl font-bold">MyWebsite</h1>
+        <h1 className="text-white text-xl font-bold">dotshowcase</h1>
         <ul className="flex space-x-6 text-gray-300">
           <li>
             <NavLink
